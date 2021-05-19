8f7625767428298bf289ecc1f76e1bfae521506c
Martial DAMBIA <dambiamartial@gmail.com>
2021-05-19 12:20:50 +0100

    Définition des abstractions et évènements

---
+++ Sigma.Systems.GenericEventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using Sigma.Systems.GenericEventBus.Events;
+using System.Threading.Tasks;
+
+namespace Sigma.Systems.GenericEventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
